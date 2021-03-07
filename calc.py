9bd05739953d84e457a15957cacb933365b6d156
Madhawa Monarawila <madawammonarawila@gmail.com>
2021-03-07 18:07:17 +0530

    add comments in line 7-12

--- calc.py
+++ calc.py
@@ -4,7 +4,13 @@ f = open('first.txt')
 lines = f.readlines()
 # print(lines[32].index("TB"))
 line = lines[32]
-#tb_location = line.index("TB")
+'''
+If your code isn't working as expected, 
+use the commented out code to find the index of you TB value. 
+update line 15 and 20 accordingly. 
+.strip method will remove the extra bit from the selected part.
+'''
+# tb_location = line.index("TB")
 # print(tb_location)
 first_read_from_file = (line[45:52]).strip(" TB[]")
 
@@ -22,7 +28,7 @@ drive_size = 256  # (GB)
 avail_space = float(input("Enter the current available Free Space on your SSD in GBs : "))  # free space available (GB)
 life_time = round(150/256*avail_space /((new_read - first_read)*365/time_period), 2)
 
-# from purchasing date
+# counting from purchasing date
 print(f"You have at least {life_time} years of lifetime")
 
 f.close()
