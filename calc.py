60d4a743a2926ec1bfc8df524e04a173771d314e
Madhawa Monarawila <madawammonarawila@gmail.com>
2021-03-14 11:58:21 +0530

    rename variables for better readability

--- calc.py
+++ calc.py
@@ -1,36 +1,35 @@
 import os
 os.system("smartctl -a disk0 > ~/newssdlc.txt")
-f = open('firstssdlc.txt')
-lines = f.readlines()
-# print(lines[32].index("TB"))
-line = lines[32]
+
+
 '''
 If your code isn't working as expected, 
 use the commented out code to find the index of you TB value. 
 update line 15 and 20 accordingly. 
 .strip method will remove the extra bit from the selected part.
 '''
-# tb_location = line.index("TB")
-# print(tb_location)
-first_read_from_file = (line[45:52]).strip(" TB[]")
 
-g = open('newssdlc.txt')
-lines_2 = g.readlines()
-line_2 = lines_2[32]
-new_read_from_file = (line_2[45:52]).strip(" TB[]")
+first_file = open('firstssdlc.txt')
+all_lines_of_first_file = first_file.readlines()
+line_with_data_from_fristfile = all_lines_of_first_file[32]
+# print(lines[32].index("TB")) 
+data_value_from_first_file = (line_with_data_from_fristfile[45:52]).strip(" TB[]")
+first_data_value = float(data_value_from_first_file)
 
-first_read = float(first_read_from_file)
-# first_read = 1.47  # (TB)
-new_read = float(new_read_from_file)
-# new_read = 1.61  # (TB)
+new_file = open('newssdlc.txt')
+all_lines_of_new_file = new_file.readlines()
+line_with_data_from_newfile = all_lines_of_new_file[32]
+data_value_from_new_file = (line_with_data_from_newfile[45:52]).strip(" TB[]")
+new_data_value = float(data_value_from_new_file)
+
 time_period = float(input("No. of Days from the creation of firstssdlc.txt file? : "))  # in days
-drive_size = 256  # (GB)
+#drive_size = 256  # (GB)
 avail_space = float(input("Enter the current available Free Space on your SSD in GBs : "))  # free space available (GB)
-life_time = round(150/256*avail_space /((new_read - first_read)*365/time_period), 2)
+life_time = round(150/256*avail_space /((new_data_value - first_data_value)*365/time_period), 2)
 
 # counting from purchasing date
 print(f"You have at least {life_time} years of lifetime")
 
-f.close()
-g.close()
+first_file.close()
+new_file.close()
 input("Press \"Enter\" to Quit.")
